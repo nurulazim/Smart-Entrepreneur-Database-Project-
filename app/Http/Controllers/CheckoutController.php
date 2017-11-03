8f241541f92150489614e86628bf3c1b3ad80cee
nurulazim <rizvisust2013@gmail.com>
2017-11-03 17:32:42 +0600

    Initial commit

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use Auth;
+use Illuminate\Http\Request;
+
+class CheckoutController extends Controller
+{
+    public function step1()
+    {
+    	if(Auth::check()){
+    		return redirect()->route('checkout.shipping');
+    	}
+    	return redirect('login');
+    }
+    public function shipping()
+    {
+    	return view('front.shipping-info');
+    }
+}
---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class FrontController extends Controller
+{
+    public function index()
+    {
+        $shirts=Product::all();
+    	return view('front.home', compact('shirts'));
+    }
+
+    public function shirts()
+    {
+        $shirts=Product::all();
+    	return view('front.shirts',compact('shirts'));
+    }
+
+    public function shirt()
+    {
+        $shirt=Product::all();
+    	return view('front.shirt',compact('shirt'));
+    }
+}
